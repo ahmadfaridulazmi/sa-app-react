c81c8ff709a90a4320873c18520529fbeb2c4416
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2020-10-23 07:25:04 +0800

    fix env_varible

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const ENV_VARS = process.env;
 const config = {
-    BASE_URL: ENV_VARS.BASE_URL || 'http://localhost:4000'
+    BASE_URL: ENV_VARS.REACT_APP_BASE_URL || 'http://localhost:4000'
 }
 
 module.exports = config;
